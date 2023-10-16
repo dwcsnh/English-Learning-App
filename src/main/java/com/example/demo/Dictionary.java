978db688f77446b7450a34bd9fe49ba76679d63b
DTB2324 <dotrongbinh20012004@gmail.com>
2023-10-16 16:26:50 +0700

    1626

--- src/main/java/com/example/demo/Dictionary.java
+++ src/main/java/com/example/demo/Dictionary.java
@@ -4,12 +4,24 @@ import java.util.ArrayList;
 
 public class Dictionary {
     private ArrayList<Word> wordList = new ArrayList<>();
+    private ArrayList<String> wordTargetList = new ArrayList<>();
+    private ArrayList<String> wordExplainList = new ArrayList<>();
 
     public void addWord(Word word) {
         wordList.add(word);
+        wordTargetList.add(word.getWordTarget());
+        wordExplainList.add(word.getWordExplain());
     }
 
     public ArrayList<Word> getWordList() {
         return wordList;
+    }
+
+    public ArrayList<String> getWordTargetList() {
+        return wordTargetList;
+    }
+
+    public ArrayList<String> getWordExplainList() {
+        return wordExplainList;
     }
 }
