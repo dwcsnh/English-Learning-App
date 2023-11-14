f220e7d2f684ae4fce4a1ba792a1e64c96c32562
DTB2324 <dotrongbinh20012004@gmail.com>
2023-11-14 21:53:54 +0700

    Wordle Game !

---
+++ src/main/java/com/example/demo/WordleController.java
@@ -0,0 +1,192 @@
+package com.example.demo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.io.*;
+import java.util.Random;
+import java.util.regex.Pattern;
+
+public class WordleController {
+    @FXML
+    private Label label1 = new Label();
+    @FXML
+    private Label label2 = new Label();
+    @FXML
+    private Label label3 = new Label();
+    @FXML
+    private Label label4 = new Label();
+    @FXML
+    private Label label5 = new Label();
+    @FXML
+    private Label label6 = new Label();
+    @FXML
+    private Label label7 = new Label();
+    @FXML
+    private Label label8 = new Label();
+    @FXML
+    private Label label9 = new Label();
+    @FXML
+    private Label label10 = new Label();
+    @FXML
+    private Label label11 = new Label();
+    @FXML
+    private Label label12 = new Label();
+    @FXML
+    private Label label13 = new Label();
+    @FXML
+    private Label label14 = new Label();
+    @FXML
+    private Label label15 = new Label();
+    @FXML
+    private Label label16 = new Label();
+    @FXML
+    private Label label17 = new Label();
+    @FXML
+    private Label label18 = new Label();
+    @FXML
+    private Label label19 = new Label();
+    @FXML
+    private Label label20 = new Label();
+    @FXML
+    private Label label21 = new Label();
+    @FXML
+    private Label label22 = new Label();
+    @FXML
+    private Label label23 = new Label();
+    @FXML
+    private Label label24 = new Label();
+    @FXML
+    private Label label25 = new Label();
+    @FXML
+    private Label label26 = new Label();
+    @FXML
+    private Label label27 = new Label();
+    @FXML
+    private Label label28 = new Label();
+    @FXML
+    private Label label29 = new Label();
+    @FXML
+    private Label label30 = new Label();
+    @FXML
+    private TextField textField;
+    @FXML
+    private Button button;
+
+    private static final String DATA_WordleWord_PATH = "data\\wordleWord.txt";
+    String[] words;
+
+    String ans;
+    private int round = 0;
+    private boolean win = false;
+    private boolean end = false;
+
+    public WordleController() throws IOException {
+        InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(DATA_WordleWord_PATH), "UTF8");
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            words = line.split(" ");
+            for(String word : words) {
+                System.out.println(word);
+            }
+        }
+        Random random = new Random();
+        ans = words[random.nextInt(words.length)].toUpperCase();
+    }
+
+
+
+    @FXML
+    public void checkGuess(ActionEvent event) {
+        Label[] labels = {
+                label1, label2, label3, label4, label5, label6, label7, label8, label9, label10,
+                label11, label12, label13, label14, label15, label16, label17, label18, label19, label20,
+                label21, label22, label23, label24, label25, label26, label27, label28, label29, label30
+        };
+        String guess = textField.getText().toUpperCase();
+        if (guess.length() != 5 || Pattern.matches(".*[\\W\\d].*", guess)) {
+            showAlert("Invalid input", "Please type down 5 letter word without number or specific character!!");
+            textField.clear();
+            return;
+        }
+        for (int i = 0; i < guess.length(); i++) {
+            String letter = guess.substring(i, i + 1);
+            labels[i + round * 5].setText(letter);
+            System.out.println(labels[i + round * 5].getText());
+            if (letter.equals(ans.substring(i, i + 1))) {
+                labels[i + round * 5].setStyle("-fx-background-radius: 5 5 5 5;" +
+                                                "-fx-background-color: #538D4E;");
+            } else if (ans.contains(letter)) {
+                labels[i + round * 5].setStyle("-fx-background-color: #B59F3B;" +
+                                                "-fx-background-radius: 5 5 5 5");
+            } else {
+                labels[i + round * 5].setStyle("-fx-background-color: #3A3A3C;" +
+                                                "-fx-background-radius: 5 5 5 5;");
+            }
+        }
+        if (guess.equals(ans)) {
+            win = true;
+        }
+        System.out.println("end: " + end);
+        if (!win) {
+            if (!end) {
+                nextRound();
+            } else {
+                System.out.println("You lose!!");
+                showAlert("You lose!", "The correct word was: " + ans);
+                // Do something : show answer, alert, ...
+                for (int i = 0; i < 30; i++) {
+                    labels[i].setText("");
+                    labels[i].setStyle("-fx-background-color: #121213;" +
+                            "-fx-border-color: #3A3A3C;" +
+                            "-fx-border-width: 5;" +
+                            "-fx-border-radius: 5 5 5 5");
+                }
+                reset();
+            }
+        } else {
+            System.out.println("You win!!");
+            // Do something before start a new game
+            showAlert("You win!", "Congratulations!\n" + "The correct word was: " + ans);
+
+            //reset
+            for (int i = 0; i < 30; i++) {
+                labels[i].setText("");
+                labels[i].setStyle("-fx-background-color: #121213;" +
+                        "-fx-border-color: #3A3A3C;" +
+                        "-fx-border-width: 5;");
+            }
+            reset();
+            //reset
+        }
+        textField.clear();
+    }
+
+    private void showAlert(String title, String content) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+
+    public void nextRound() {
+        round++;
+        if (round > 4) {
+            end = true;
+        }
+    }
+
+    public void reset() {
+        round = 0;
+        win = false;
+        end = false;
+        Random random = new Random();
+        ans = words[random.nextInt(words.length)].toUpperCase();
+    }
+}
